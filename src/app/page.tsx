be222d9653dddb7e92eda318d87332d8836d17f3
krostyle <diegoo.albarran@gmail.com>
2024-02-26 01:44:34 -0300

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
 import LoginForm from "@/infrastructure/ui/components/molecules/LoginForm";
 import Navbar from "@/infrastructure/ui/components/organisms/Navbar";
+import Footer from "@/infrastructure/ui/components/organisms/Footer";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-global">
+    <main className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="flex flex-col items-center justify-between p-24">
-        <div className="w-full max-w-md mt-8">
-          <LoginForm />
+      <div className="flex-grow">
+        <div className="flex flex-col items-center justify-between p-24">
+          <div className="w-full max-w-md mt-8">
+            <LoginForm />
+          </div>
         </div>
       </div>
+      {/* Footer */}
+      <Footer />
     </main>
   );
 }
